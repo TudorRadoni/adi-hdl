5ed5f0abf53b56bf607639601e357ff9c8e05c75
Dragos Popa <dragos.popa777@gmail.com>
2023-08-02 21:51:13 +0300

    library/axi_pwm_custom : IP for custom PWM generator
    Interface for custom PWM generator
    The duration of the PWM signal is determined by comparing an input value to a sawtooth signal.
    Sawtooth signal is created by incrementing a 12-bit value to maximum than reseting it to 0.
    The PWM signal pulse lasts until the value of the sawtooth signal is greater than the input value.
    The PWM signal controls the brightness of four LEDs.

--- library/axi_pwm_custom/axi_pwm_custom_if.v
+++ library/axi_pwm_custom/axi_pwm_custom_if.v
@@ -48,42 +48,94 @@ module axi_pwm_custom_if (
   output           pwm_led_1,
   output           pwm_led_2,
   output           pwm_led_3
+  
 );
 
   localparam PULSE_PERIOD = 4095;
 
   // internal registers
 
+  reg    [11:0]  pulse_period_cnt = 12'b0;;
+  reg    [11:0]  current_data_ch_0;
+  reg    [11:0]  current_data_ch_1;
+  reg    [11:0]  current_data_ch_2;
+  reg    [11:0]  current_data_ch_3;
+  reg            pwm_led_0_value = 1'b0;
+  reg            pwm_led_1_value = 1'b0;
+  reg            pwm_led_2_value = 1'b0;
+  reg            pwm_led_3_value = 1'b0;
 
   // internal wires
 
-  wire           end_of_period;  
-
+  wire            end_of_period; 
 
 // generate a signal named end_of_period witch has '1' logic value at the end of the signal period
 
-  assign end_of_period = (/*condition here*/) ? 1'b1 : 1'b0;
+  assign end_of_period = (pulse_period_cnt == PULSE_PERIOD) ? 1'b1 : 1'b0;
+
+  assign pwm_led_0 = pwm_led_0_value;
+  assign pwm_led_1 = pwm_led_1_value;
+  assign pwm_led_2 = pwm_led_2_value;
+  assign pwm_led_3 = pwm_led_3_value;
 
 // Create a counter from 0 to PULSE_PERIOD
 
   always @(posedge pwm_clk) begin
-   
+    if(end_of_period || !rstn) begin
+      pulse_period_cnt <= 12'b0;
+    end else begin
+      pulse_period_cnt <= pulse_period_cnt + 1'b1;
+    end
   end
 
 // control the pwm signal value based on the input signal and counter value
 
-  always @(posedge pwm_clk) begin
-    
-  end  
+
+  //assign pwm_led_0 = (pulse_period_cnt <= current_data_ch_0) ? 1'b1 : 1'b0;
+  //assign pwm_led_1 = (pulse_period_cnt <= current_data_ch_1) ? 1'b1 : 1'b0;
+  //assign pwm_led_2 = (pulse_period_cnt <= current_data_ch_2) ? 1'b1 : 1'b0;
+  //assign pwm_led_3 = (pulse_period_cnt <= current_data_ch_3) ? 1'b1 : 1'b0;
+
+ always @(posedge pwm_clk) begin
+    if (pulse_period_cnt <= current_data_ch_0) begin
+      pwm_led_0_value <= 1'b1;
+    end else begin
+      pwm_led_0_value <= 1'b0;
+    end
+
+    if (pulse_period_cnt <= current_data_ch_1) begin
+      pwm_led_1_value <= 1'b1;
+    end else begin
+      pwm_led_1_value <= 1'b0;
+    end
+
+    if (pulse_period_cnt <= current_data_ch_2) begin
+      pwm_led_2_value <= 1'b1;
+    end else begin
+      pwm_led_2_value <= 1'b0;
+    end
+
+    if (pulse_period_cnt <= current_data_ch_3) begin
+      pwm_led_3_value <= 1'b1;
+    end else begin
+      pwm_led_3_value <= 1'b0;
+    end
+  end
 
 // make sure that the new data is processed only after the END_OF_PERIOD
 
   always @(posedge pwm_clk) begin
   
     if (end_of_period) begin
-  
+      current_data_ch_0 <= data_channel_0;
+      current_data_ch_1 <= data_channel_1;
+      current_data_ch_2 <= data_channel_2;
+      current_data_ch_3 <= data_channel_3;
     end else begin
-  
+      current_data_ch_0 <= current_data_ch_0;
+      current_data_ch_1 <= current_data_ch_1;
+      current_data_ch_2 <= current_data_ch_2;
+      current_data_ch_3 <= current_data_ch_3;
     end 
   end
 
