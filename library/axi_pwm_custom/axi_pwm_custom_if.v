18edff9cf93a82f17be16418c05d039f714d18d1
U-DESKTOP-KVT6A15\alexb <alexbocioanca123@gmail.com>
2023-08-03 01:18:01 +0300

    library\axi_pwm_custom: Add AXI PWM Custom Module with Interface - Implemented an AXI PWM custom module with an interface to create PWM output signals and connect them to an AXI bus. - The module includes internal registers, clocks, and signals for handling data channels and interfaces. - Each channel is linked to corresponding data sources and output LEDs for effective signal generation.

--- library/axi_pwm_custom/axi_pwm_custom_if.v
+++ library/axi_pwm_custom/axi_pwm_custom_if.v
@@ -44,47 +44,102 @@ module axi_pwm_custom_if (
   input    [11:0]  data_channel_1,
   input    [11:0]  data_channel_2,
   input    [11:0]  data_channel_3,
-  output           pwm_led_0,
-  output           pwm_led_1,
-  output           pwm_led_2,
-  output           pwm_led_3
+  output    reg    pwm_led_0,
+  output    reg    pwm_led_1,
+  output    reg    pwm_led_2,
+  output    reg    pwm_led_3
 );
 
   localparam PULSE_PERIOD = 4095;
 
   // internal registers
 
-
+  reg   [11:0]  counter = 12'h0;
+  reg   [11:0]  current_data_channel_0 = 12'h0;
+  reg   [11:0]  current_data_channel_1 = 12'h0;
+  reg   [11:0]  current_data_channel_2 = 12'h0;
+  reg   [11:0]  current_data_channel_3 = 12'h0;
   // internal wires
 
-  wire           end_of_period;  
-
+  wire          end_of_period;  
 
 // generate a signal named end_of_period witch has '1' logic value at the end of the signal period
 
-  assign end_of_period = (/*condition here*/) ? 1'b1 : 1'b0;
+  assign end_of_period = (counter == PULSE_PERIOD) ? 1'b1 : 1'b0;
 
 // Create a counter from 0 to PULSE_PERIOD
 
   always @(posedge pwm_clk) begin
-   
+    if(end_of_period == 1'b1) begin
+      counter <= 12'd0;
+      current_data_channel_0 <= data_channel_0;
+      current_data_channel_1 <= data_channel_1;
+      current_data_channel_2 <= data_channel_2;
+      current_data_channel_3 <= data_channel_3;
+    end else begin
+      counter <= counter + 1'b1;
+    end
   end
 
 // control the pwm signal value based on the input signal and counter value
-
+  
   always @(posedge pwm_clk) begin
-    
-  end  
-
-// make sure that the new data is processed only after the END_OF_PERIOD
-
-  always @(posedge pwm_clk) begin
-  
-    if (end_of_period) begin
-  
-    end else begin
-  
-    end 
+          
+    if (counter < current_data_channel_0)
+      begin
+        pwm_led_0 <=1'b1;
+      end
+    else
+      if (counter > current_data_channel_0)
+      begin
+        pwm_led_0 <=1'b0;
+      end
+      else
+        begin
+          pwm_led_0 <= pwm_led_0;
+        end
+          
+    if (counter < current_data_channel_1)
+      begin
+        pwm_led_1 <=1'b1;
+      end
+    else
+      if (counter > current_data_channel_1)
+      begin
+        pwm_led_1 <=1'b0;
+      end
+      else
+        begin
+          pwm_led_1 <= pwm_led_1;
+        end
+          
+    if (counter < data_channel_2)
+      begin
+        pwm_led_2 <=1'b1;
+      end
+    else
+      if (counter > data_channel_2)
+      begin
+        pwm_led_2 <=1'b0;
+      end
+      else
+        begin
+          pwm_led_2 <= pwm_led_2;
+        end
+          
+    if (counter < data_channel_3)
+      begin
+        pwm_led_3 <=1'b1;
+      end
+    else
+      if (counter > data_channel_3)
+      begin
+        pwm_led_3 <=1'b0;
+      end
+      else
+        begin
+          pwm_led_3 <= pwm_led_3;
+        end
   end
 
 endmodule
