f6d6e8f74e0e6cfa5cb8e661662023c556772973
Stefan <stavridominic@yahoo.com>
2023-08-03 10:06:49 +0300

    library/verilog_task_testbench : generate triangle wave
    In "verilog_task_testbench.v" file I have created a module called verilog_task_testbench that generated a triangle wave signal.

--- library/verilog_task_testbench/verilog_task_testbench_tb.v
+++ library/verilog_task_testbench/verilog_task_testbench_tb.v
@@ -32,12 +32,19 @@
 //
 // ***************************************************************************
 // ***************************************************************************
-
 `timescale 1ns/100ps
-
 module verilog_task_testbench_tb;
   parameter VCD_FILE = "verilog_task_testbench_tb.vcd";
-
   `define TIMEOUT 900
-
+  wire [11:0] triangle_wave;
+  reg pwm_clk = 1'b0;
+  reg resetn_in = 1'b0;
+  always #1 pwm_clk = !pwm_clk;
+  initial begin
+    #1000 resetn_in = 1'b1;
+  end
+  verilog_task_testbench  verilog_task_testbench_inst (
+    .ref_clk(pwm_clk),
+    .rstn(resetn_in),
+    .triangle_wave(triangle_wave));
 endmodule
