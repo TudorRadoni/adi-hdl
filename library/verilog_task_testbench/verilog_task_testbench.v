9cb22b4d567d4916bee65a5a166d14f19c988665
U-DESKTOP-NUI87KK\Voic <voicandrei22@gmail.com>
2023-08-03 09:23:01 +0300

    Implement triangle wave generator in Verilog
    - Created a triangle wave generator module to help with familiarity in Verilog.
    - Included testbenches and simulation results for verification.

--- library/verilog_task_testbench/verilog_task_testbench.v
+++ library/verilog_task_testbench/verilog_task_testbench.v
@@ -1,46 +1,34 @@
-// ***************************************************************************
-// ***************************************************************************
-// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
-//
-// In this HDL repository, there are many different and unique modules, consisting
-// of various HDL (Verilog or VHDL) components. The individual modules are
-// developed independently, and may be accompanied by separate and unique license
-// terms.
-//
-// The user should read each of these license terms, and understand the
-// freedoms and responsibilities that he or she has by using this source/core.
-//
-// This core is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-// A PARTICULAR PURPOSE.
-//
-// Redistribution and use of source or resulting binaries, with or without modification
-// of this file, are permitted under one of the following two license terms:
-//
-//   1. The GNU General Public License version 2 as published by the
-//      Free Software Foundation, which can be found in the top level directory
-//      of this repository (LICENSE_GPL2), and also online at:
-//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
-//
-// OR
-//
-//   2. An ADI specific BSD license, which can be found in the top level directory
-//      of this repository (LICENSE_ADIBSD), and also on-line at:
-//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
-//      This will allow to generate bit files and not release the source code,
-//      as long as it attaches to an ADI device.
-//
-// ***************************************************************************
-// ***************************************************************************
-// This is the LVDS/DDR interface
-
 `timescale 1ns/100ps
 
 module verilog_task_testbench ( 
 
   input            ref_clk,
   input            rstn,
-  output   [11:0]  triangle_wave
+  output [11:0]  triangle_wave
 );
+
+  reg [11:0] count;
+  reg dir; // direction control bit, 0 for up, 1 for down
+
+  always @(posedge ref_clk or negedge rstn) begin
+    if (!rstn) begin
+      count <= 12'd0; // reset count to 0
+      dir <= 1'b0; // set direction to up
+    end else begin
+      if (dir == 1'b0) begin
+        if (count == 12'd4095) // if count reached max value
+          dir <= 1'b1; // change direction to down
+        else
+          count <= count + 12'd1;
+      end else begin
+        if (count == 12'd0) // if count reached min value
+          dir <= 1'b0; // change direction to up
+        else
+          count <= count - 12'd1;
+      end
+    end
+  end
+
+  assign triangle_wave = count;
 
 endmodule
